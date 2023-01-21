c65dfb8303b06db2e3ec598654a197c9d6a88e50
joel ibaceta <mail@joelibaceta.com>
2023-01-21 08:36:49 -0600

    using invitation codes

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -13,38 +13,38 @@ import {
 } from "../component";
 import { Header, Footer } from "../component/shared";
 
-// import { API } from "../api";
+import { API } from "../api";
 
 export default function Home() {
   const navigate = useNavigate();
   const [loading, setLoading] = useState<boolean>(true);
   const [showOverlay, setShowOverlay] = useState<boolean>(false);
 
-  // const searchParams = new URLSearchParams(window.location.search);
+  const searchParams = new URLSearchParams(window.location.search);
 
   useEffect(() => {
     (async () => {
       try {
-        // const response = await fetch(`${API}/invitation_code.py`, {
-        //   method: "POST",
-        //   body: searchParams.get("invitation_code")
-        //     ? JSON.stringify({
-        //         invitation_code: searchParams.get("invitation_code"),
-        //       })
-        //     : JSON.stringify({}),
-        // });
+        const response = await fetch(`${API}/invitation_code.py`, {
+          method: "POST",
+          body: searchParams.get("invitation_code")
+            ? JSON.stringify({
+                invitation_code: searchParams.get("invitation_code"),
+              })
+            : JSON.stringify({}),
+        });
 
-        // const data: string = await response.text();
+        const data: string = await response.text();
 
-        // if (
-        //   data === "Invalid invitation_code" ||
-        //   data === "No invitation_code"
-        // ) {
-        setShowOverlay(false);
-        //   setLoading(false);
-        // }
+        if (
+          data === "Invalid invitation_code" ||
+          data === "No invitation_code"
+        ) {
+          setShowOverlay(false);
+          setLoading(false);
+        }
 
-        setLoading(false);
+        //setLoading(false);
       } catch (error: any) {
         setLoading(false);
         navigate("/server-error");
